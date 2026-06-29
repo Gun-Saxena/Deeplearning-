fe02175b1aa13c17c24f003c58e1783cafd236f6
Gun-Saxena <gunsaxena2004@gmail.com>
2026-06-29 13:41:22 +0530

    updated

--- engineering_concepts/design_pattern/SingletonPattern/Logger.java
+++ engineering_concepts/design_pattern/SingletonPattern/Logger.java
@@ -1,17 +1,14 @@
 package engineering_concepts.design_pattern.SingletonPattern;
 
 public class Logger {
-    private static Logger instance;
+    private static Logger obj;
     private Logger(){
         System.out.println("Logger instance created.");
     }
     public static Logger getInstance(){
-        if (instance == null) {
-            instance = new Logger();
+        if (obj == null) {
+            obj= new Logger();
         }
-        return instance;
-    }
-    public void log(String message) {
-        System.out.println("Log: " + message);
+        return obj;
     }
 }
