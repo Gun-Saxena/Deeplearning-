9cbd326fafcd263cc7592431004b4a62ddccdb69
Gun-Saxena <gunsaxena2004@gmail.com>
2026-06-29 13:22:24 +0530

    Updated

--- engineering_concepts/design_pattern/AdapterPattern/PayPalGateway.java
+++ engineering_concepts/design_pattern/AdapterPattern/PayPalGateway.java
@@ -2,7 +2,6 @@ package engineering_concepts.design_pattern.AdapterPattern;
 
 public class PayPalGateway {
     public void sendPayment(double amt) {
-        System.out.println("Payment of Rs"+ amt+" processed using PayPal");
+        System.out.println("Payment by using PayPal "+ amt);
     }
-
 }
