85a48946e2b3281214c0d2f5e985ba294de4a64b
Gun-Saxena <gunsaxena2004@gmail.com>
2026-06-29 13:21:15 +0530

    updated

--- engineering_concepts/design_pattern/AdapterPattern/Main.java
+++ engineering_concepts/design_pattern/AdapterPattern/Main.java
@@ -3,8 +3,8 @@ package engineering_concepts.design_pattern.AdapterPattern;
 public class Main {
     public static void main(String[] args){
         PaymentProcessor p=new PayPalAdapter();
-        p.processPayment(1000);
+        p.processPayment(5000);
         PaymentProcessor r=new RazorpayAdapter();
-        r.processPayment(3000);
+        r.processPayment(2000);
     }
 }
