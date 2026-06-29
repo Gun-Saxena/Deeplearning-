173857667ca0e7a299af81bd8efadc42b90c0d1e
Gun-Saxena <gunsaxena2004@gmail.com>
2026-06-29 13:42:36 +0530

    updated
    Updated log messages to improve clarity in output.

--- engineering_concepts/design_pattern/SingletonPattern/Main.java
+++ engineering_concepts/design_pattern/SingletonPattern/Main.java
@@ -2,13 +2,12 @@ package engineering_concepts.design_pattern.SingletonPattern;
 public class Main {
     public static void main(String[] args) {
         Logger l1=Logger.getInstance();
-        l1.log("First log message");
         Logger l2=Logger.getInstance();
-        l2.log("Second log message");
         if(l1==l2){
-            System.out.println("Only one Logger instance exists.");
-        } else {
-            System.out.println("Multiple Logger instances exist.");
+            System.out.println("Only one Logger instance exists");
+        } 
+        else {
+            System.out.println("Multiple Logger instances exist");
         }
     }
 }
