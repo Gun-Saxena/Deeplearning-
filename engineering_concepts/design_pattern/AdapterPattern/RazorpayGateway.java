b6acc766b525f941096a4e60fa9dab0c3b2d94ac
Gun-Saxena <gunsaxena2004@gmail.com>
2026-06-29 13:23:39 +0530

    Updated

--- engineering_concepts/design_pattern/AdapterPattern/RazorpayGateway.java
+++ engineering_concepts/design_pattern/AdapterPattern/RazorpayGateway.java
@@ -2,7 +2,6 @@ package engineering_concepts.design_pattern.AdapterPattern;
 
 public class RazorpayGateway {
     public void pay(double amt) {
-        System.out.println("Payment of Rs"+amt+" processed using Razorpay");
+        System.out.println("Payment by using Razorpay "+amt);
     }
-
 }
